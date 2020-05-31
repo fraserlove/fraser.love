7495c32a90838f4775ea3aa67ad2ff0aef73bf48
Fraser Love <me@fraserlove.com>
2020-05-31 20:18:22 +0100

    Added www to root redirect

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_mail import Mail, Message
 from threading import Thread
-from urllib import urlparse, urlunparse
+from urllib import parse
 import os, datetime
 
 app = Flask(__name__)
