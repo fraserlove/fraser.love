f5324ecbb881ad509e03f7f95054812a69cd22f5
Fraser Love <me@fraser.love>
2020-06-03 01:20:23 +0100

    Removed flask caching

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_mail import Mail, Message
 from flask_compress import Compress
-from flask_caching import Cache
 from threading import Thread
 from urllib.parse import urlparse, urlunparse
 import os, datetime
@@ -17,7 +16,6 @@ app.config['MAIL_PASSWORD'] = os.environ.get('SENDER_PASS')
 
 mail = Mail(app)
 Compress(app)
-cache = Cache(app)
 
 @app.before_request
 def redirect_https():
